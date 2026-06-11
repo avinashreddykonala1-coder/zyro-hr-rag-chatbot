65fdac4b5fdae7e16ce2d7e989349ed06f609a05
Avinash Reddy Konala <avinashreddykonala1@gmail.com>
2026-06-11 14:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def build_rag():
 
     llm = ChatGroq(
         groq_api_key=st.secrets["GROQ_API_KEY"],
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         temperature=0
     )
 
